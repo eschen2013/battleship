b805012a2e04f9accc8d7649ae675817fdce5977
Carl Zulauf <carl@linkleaf.com>
2014-02-16 23:53:20 -0500

    Base stat bonus off game_stats analysis

--- contestants/lib/bender/strategies/stat_bonus.rb
+++ contestants/lib/bender/strategies/stat_bonus.rb
@@ -1,27 +1,9 @@
 module Bender
   module Strategies
     class StatBonus < Base
-      def initialize(*a)
-        super(*a)
-        compute_stats
-      end
-
-      def compute_stats
-        @stats = Hash.new(0)
-        games = Bender.game_history.map do |game|
-          game.map{ |ship| Ship.new(*ship) }
-        end
-        games.each do |ships|
-          ships.each do |ship|
-            ship.points.each do |xy|
-              @stats[xy] += weight
-            end
-          end
-        end
-      end
 
       def score
-        @stats.each_pair do |xy, score|
+        Bender.game_stats.each_pair do |xy, score|
           board.at(*xy).add(score)
         end
       end
